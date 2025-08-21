061fcdb5286d43d54091e34b1f2e72021c0ce934
Sami <samikhan10902@gmail.com>
2025-08-21 19:39:20 +0530

    done

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@workspace/ui/components/button"
+import { add } from "../../../packages/math/src/add"
+import { Input } from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">Hello app/web</h1>
         <Button size="sm">Button</Button>
+        <p>
+          {add(2,2)}
+        </p>
+        <Input/>
       </div>
     </div>
   )
---
+++ apps/widget/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
+        
+      </div>
+    </div>
+  )
+}
